85c5b997320cfb97e239bf0347d2335c7d651711
Tarunjit <113191713+Tarun-1907@users.noreply.github.com>
2023-07-11 13:22:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("script running......")
+document.querySelector('.cross').style.display ='none';
+document.querySelector('.hamburger').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo')
+    if(document.querySelector('.sidebar').classList.contains('sidebargo'))
+    {
+        document.querySelector('.ham').style.display ='inline';
+        document.querySelector('.cross').style.display ='none';
+    }
+    else{
+        document.querySelector('.ham').style.display ='none';
+        setTimeout(() => {
+            document.querySelector('.cross').style.display ='inline';
+        }, 330);
+    }
+})
